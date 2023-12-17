b4986ce5f70fab6ef1e939113acc479feec522ab
willson <93517421+willson1661@users.noreply.github.com>
2023-12-17 16:14:17 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,96 @@
+import React, { useState } from "react";
+import "./styles.css";
+
+const App = () => {
+  const [result, setResult] = useState("");
+
+  const handleClick = (e) => {
+    setResult(result.concat(e.target.name));
+  };
+  const clear = () => {
+    setResult("");
+  };
+  const backspace = () => {
+    setResult(result.slice(0, -1));
+  };
+  const calculate = () => {
+    try {
+      setResult(eval(result).toString());
+    } catch (err) {
+      setResult("Error");
+    }
+  };
+
+  return (
+    <section className="Total">
+    <div className="calculator-grid">
+      <div className="output">
+        <form>
+          <textarea
+            type="text"
+            value={result}
+            // style={{ fontSizeAdjust: "0.58" }}
+          ></textarea>
+        </form>
+      </div>
+      <button onClick={clear} className="span-two">
+        AC
+      </button>
+      <button onClick={backspace}>DEL</button>
+      <button name="/" onClick={handleClick}>
+        ÷
+      </button>
+
+      <button name="1" onClick={handleClick}>
+        1
+      </button>
+      <button name="2" onClick={handleClick}>
+        2
+      </button>
+      <button name="3" onClick={handleClick}>
+        3
+      </button>
+      <button name="*" onClick={handleClick}>
+        *
+      </button>
+      <button name="4" onClick={handleClick}>
+        4
+      </button>
+      <button name="5" onClick={handleClick}>
+        5
+      </button>
+      <button name="6" onClick={handleClick}>
+        6
+      </button>
+      <button name="+" onClick={handleClick}>
+        +
+      </button>
+
+      <button name="7" onClick={handleClick}>
+        7
+      </button>
+      <button name="8" onClick={handleClick}>
+        8
+      </button>
+      <button name="9" onClick={handleClick}>
+        9
+      </button>
+      <button name="-" onClick={handleClick}>
+        -
+      </button>
+      <button name="." onClick={handleClick}>
+        .
+      </button>
+      <button name="0" onClick={handleClick}>
+        0
+      </button>
+
+      <button onClick={calculate} className="span-two">
+        =
+      </button>
+    </div>
+      </section>
+  );
+};
+
+export default App;
